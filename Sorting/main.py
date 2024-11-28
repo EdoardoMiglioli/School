99bbecb5cb30ef196e266d3e16fa14d20356e58b
Edoardo Miglioli <edoardomiglioli2008@gmail.com>
2024-11-28 07:35:03 +0100

    sorting 3 nums fro lowest to higher

---
+++ Sorting/main.py
@@ -0,0 +1,12 @@
+def sort_three_numbers(a, b, c):
+    if a > b:
+        a, b = b, a
+    if b > c:
+        b, c = c, b
+    if a > b:
+        a, b = b, a
+    return a, b, c
+
+x, y, z = map(int, input('Enter three numbers separated by spaces: ').split())
+sorted_numbers = sort_three_numbers(x, y, z)
+print('Sorted numbers: ', sorted_numbers)
