5a1993e71e1e0a84f416f7e61d3e4360df82c4dd
Edoardo Miglioli <edoardomiglioli2008@gmail.com>
2024-11-30 08:44:52 +0100

    updated sorting 3 nums

--- Sorting/main.py
+++ Sorting/main.py
@@ -10,3 +10,9 @@ def sort_three_numbers(a, b, c):
 x, y, z = map(int, input('Enter three numbers separated by spaces: ').split())
 sorted_numbers = sort_three_numbers(x, y, z)
 print('Sorted numbers: ', sorted_numbers)
+
+"""
+array_ = list(map(int, input('Enter three numbers separated by spaces: ').split()))
+array_.sort()
+print(array_)
+"""
