9aece46b36ac1f25ae2e1f58d6f59481a5c494e4
Edoardo Miglioli <edoardomiglioli2008@gmail.com>
2024-12-12 20:34:00 +0100

    Python Exercies 7

---
+++ First-n-even-nums/main.py
@@ -0,0 +1,5 @@
+nums = int(input('How many even numbers do you want? '))
+current_num = 0
+for num in range(0, nums):
+    current_num += 2
+    print(current_num)
---
+++ Odd_nums_less_than_N/main.py
@@ -0,0 +1,5 @@
+input_num = int(input('Enter your number: '))
+
+for num in range(input_num):
+    if num % 2 == 1:
+        print(num)
